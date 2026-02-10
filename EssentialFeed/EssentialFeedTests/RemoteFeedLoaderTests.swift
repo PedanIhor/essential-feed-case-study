e04072dc352d349d1964056030aa2d8d92ed3009
Pedan Ihor <ihorpedan@icloud.com>
2026-02-10 10:58:15 +0100

    Add RemoteFeedLoader Result type with success/failure

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -42,7 +42,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         expect(sut, toCompleteWithError: .connectivity) {
             let clientError = NSError(domain: "test", code: 0)
             client.complete(with: clientError)
-        }        
+        }
     }
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
@@ -81,12 +81,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { capturedResults.append($0) }
         
         action()
         
-        XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+        XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
     }
     
     private class HTTPClientSpy: HTTPClient {
