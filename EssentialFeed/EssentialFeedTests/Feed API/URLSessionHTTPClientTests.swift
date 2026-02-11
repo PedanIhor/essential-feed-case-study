3836d53350fdecd36140b7da421b001836d4437f
Pedan Ihor <ihorpedan@icloud.com>
2026-02-11 15:42:52 +0100

    Make URLSessionHTTPClient conform to the HTTPClient protocol

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import EssentialFeed
 
-class URLSessionHTTPClient {
+class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
     init(session: URLSession = .shared) {
@@ -95,7 +95,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let client = URLSessionHTTPClient()
         trackForMemoryLeaks(client, file: file, line: line)
         return client
