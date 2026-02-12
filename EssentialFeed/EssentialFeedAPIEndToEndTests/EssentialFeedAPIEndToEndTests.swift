2a5eb957e0b182fa306b3ed5fc742dc0d7ebed7d
Pedan Ihor <ihorpedan@icloud.com>
2026-02-12 11:26:35 +0100

    Changed bundle ID and added a new test target for end to end tests

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Ihor Pedan on 12/02/2026.
+//
+
+import XCTest
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+}
